2b08d8588ec4ac242400a241a6b384de22384265
雷婉悦 <leiwanyue@gtcom.com.cn>
2021-07-28 17:51:28 +0800

    用户留存率图表

--- src/components/echart/userRetention/index.vue
+++ src/components/echart/userRetention/index.vue
@@ -1,6 +1,6 @@
 <!-- 用户留存率图表 -->
 <template>
-  <div>1111</div>
+  <div id="userRetention" class="chartInit"></div>
 </template>
 
 <script>
@@ -10,14 +10,143 @@ export default {
 
   data () {
     return {
+      chart: '',
     };
   },
 
-  mounted () { },
+  mounted () {
+    this.init()
+    this.$resize(this.init, this.chart)
+  },
 
   computed: {},
 
-  methods: {}
+  methods: {
+    init () {
+      let myChart = this.$echarts.init(document.getElementById('userRetention'))
+      this.chart = myChart
+      var BorderWidth = '10';
+      //反方向剩下部分的样式
+      var placeHolderStyle = {
+        normal: {
+          label: {
+            show: false,
+            position: "left"
+          },
+          labelLine: {
+            show: false
+          },
+          color: "rgba(0,0,0,0)",
+          borderColor: "rgba(0,0,0,0)",
+          borderWidth: 0
+        },
+      };
+      var options = {
+        series: [
+          {
+            type: 'pie',
+            clockWise: true, //顺时加载
+            hoverAnimation: false, //鼠标移入变大
+            radius: [100, 101],
+            itemStyle: {
+              normal: {
+                label: {
+                  show: false,
+                  position: 'outside'
+                },
+                labelLine: {
+                  show: false,
+                  length: 100,
+                  smooth: 0.5
+                },
+                borderWidth: 4,
+                shadowBlur: 40,
+                borderColor: "#0AADE5",
+              }
+            },
+            data: [{
+              value: 7,
+              name: '70%'
+            },
+            {
+              value: 3,
+              name: '',
+              itemStyle: placeHolderStyle
+            }
+            ]
+          },
+          {
+            type: 'pie',
+            clockWise: true,
+            hoverAnimation: false,
+            radius: [80, 81],
+            itemStyle: {
+              normal: {
+                label: {
+                  show: false
+                },
+                labelLine: {
+                  show: false,
+                  length: 100,
+                  smooth: 0.5
+                },
+                borderWidth: 8,
+                shadowBlur: 40,
+                borderColor: "#085977",
+                shadowColor: 'rgba(0, 0, 0, 0)' //边框阴影
+              }
+            },
+            data: [{
+              value: 6,
+              name: '60%'
+            },
+            {
+              value: 4,
+              name: '',
+              itemStyle: placeHolderStyle
+            }
+            ]
+          },
+          {
+            type: 'pie',
+            clockWise: true,
+            hoverAnimation: false,
+            radius: [60, 61],
+            itemStyle: {
+              normal: {
+                label: {
+                  show: false
+                },
+                labelLine: {
+                  show: false,
+                  length: 100,
+                  smooth: 0.5
+                },
+                borderWidth: 6,
+                shadowBlur: 40,
+                borderColor: "#FEB114",
+                shadowColor: 'rgba(0, 0, 0, 0)' //边框阴影
+              }
+            },
+            data: [{
+              value: 5,
+              name: '40%'
+            },
+            {
+              value: 5,
+              name: '',
+              itemStyle: placeHolderStyle
+            }
+            ]
+          },
+        ]
+      }
+
+
+      myChart.setOption(options);
+
+    }
+  }
 }
 
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,55 +1,57 @@
 <template>
-  <div class="home-container"></div>
-  <div class="home-content">
-    <div class="home-top">
-      <div class="home-top-left">
-        <div class="head">
-          <div class="left-logo">
-            <div class="title">全国数据监控</div>
-            <div class="date">
-              <span class="el-icon-timer themeColor"></span>
-              <span>{{ time }}</span>
+  <div class="home-container">
+    <div class="home-bgc"></div>
+    <div class="home-content">
+      <div class="home-top">
+        <div class="home-top-left">
+          <div class="head">
+            <div class="left-logo">
+              <div class="title">全国数据监控</div>
+              <div class="date">
+                <span class="el-icon-timer themeColor"></span>
+                <span>{{ time }}</span>
+              </div>
             </div>
-          </div>
-          <div
-            class="right-data"
-            v-for="(item, index) in titleList"
-            :key="index"
-          >
-            <div :class="[`${item.icon}`, 'left-icon']"></div>
-            <div class="data">
-              <div class="themeColor title">{{ item.title }}</div>
-              <div class="dataSize">
-                <span>{{ item.data }}</span>
-                <span>{{ item.unit }}</span>
+            <div
+              class="right-data"
+              v-for="(item, index) in titleList"
+              :key="index"
+            >
+              <div :class="[`${item.icon}`, 'left-icon']"></div>
+              <div class="data">
+                <div class="themeColor title">{{ item.title }}</div>
+                <div class="dataSize">
+                  <span>{{ item.data }}</span>
+                  <span>{{ item.unit }}</span>
+                </div>
               </div>
             </div>
           </div>
+          <div class="echarts borderImg">111</div>
         </div>
-        <div class="echarts borderImg">111</div>
-      </div>
-      <div class="home-top-right">
-        <div class="userRetentionEchart borderImg">
-          <div class="leftEchart">
-            <div class="chartTitle">用户留存率</div>
-            <userRetention></userRetention>
+        <div class="home-top-right">
+          <div class="userRetentionEchart borderImg">
+            <div class="leftEchart">
+              <div class="chartTitle">用户留存率</div>
+              <userRetention></userRetention>
+            </div>
+            <div class="rightEchart">
+              <div class="chartTitle">用户分布</div>
+            </div>
           </div>
-          <div class="rightEchart">
-            <div class="chartTitle">用户分布</div>
-          </div>
+          <div class="newUserEchart borderImg"></div>
         </div>
-        <div class="newUserEchart borderImg"></div>
       </div>
-    </div>
-    <div class="home-bottom">
-      <div class="bottom-left">
-        <div
-          class="borderImg data"
-          v-for="(item, index) in bottomList"
-          :key="index"
-        ></div>
+      <div class="home-bottom">
+        <div class="bottom-left">
+          <div
+            class="borderImg data"
+            v-for="(item, index) in bottomList"
+            :key="index"
+          ></div>
+        </div>
+        <div class="bottom-right borderImg">对面是表格</div>
       </div>
-      <div class="bottom-right borderImg">对面是表格</div>
     </div>
   </div>
 </template>
@@ -139,119 +141,124 @@ export default {
 .home-container {
   width: 100%;
   height: 100%;
-  background: url('../assets/img/bg.jpg') 0% 0% / 100% 100%;
-  background-size: 100% 100%;
-  z-index: -1;
-  filter: blur(6px);
-  opacity: 0.6;
-  position: absolute;
-}
-.home-content {
-  position: relative;
-  width: 100%;
-  padding: 20px;
-  .home-top {
+  .home-bgc {
     width: 100%;
-    display: flex;
-    opacity: 1 !important;
-    z-index: 999;
-    .home-top-left {
-      width: 70%;
-      height: auto;
-      .head {
-        display: flex;
-        height: 80px;
-        .left-logo {
-          .title {
-            font-size: 27px;
-            text-shadow: rgb(255 255 255) 0px 0px 31px;
-            font-weight: 700;
-            letter-spacing: 4px;
+    height: 100%;
+    background: url('../assets/img/bg.jpg') 0% 0% / 100% 100%;
+    background-size: 100% 100%;
+    z-index: -1;
+    filter: blur(6px);
+    opacity: 0.6;
+    position: absolute;
+  }
+  .home-content {
+    position: relative;
+    z-index: 99;
+    width: 100%;
+    padding: 20px;
+    .home-top {
+      width: 100%;
+      display: flex;
+      opacity: 1 !important;
+      z-index: 999;
+      .home-top-left {
+        width: 70%;
+        height: auto;
+        .head {
+          display: flex;
+          height: 80px;
+          .left-logo {
+            .title {
+              font-size: 27px;
+              text-shadow: rgb(255 255 255) 0px 0px 31px;
+              font-weight: 700;
+              letter-spacing: 4px;
+            }
+            .date {
+              margin-top: 20px;
+              span {
+                &:first-child {
+                  font-size: 20px;
+                }
+                &:last-child {
+                  font-size: 16px;
+                  padding-left: 5px;
+                }
+              }
+            }
           }
-          .date {
-            margin-top: 20px;
-            span {
-              &:first-child {
+          .right-data {
+            margin-left: auto;
+            display: flex;
+            align-items: center;
+            .left-icon {
+              color: rgb(65, 71, 78);
+              font-size: 45px;
+              margin-right: 5px;
+            }
+            .data {
+              .title {
                 font-size: 20px;
               }
-              &:last-child {
-                font-size: 16px;
-                padding-left: 5px;
+            }
+            .dataSize {
+              span {
+                &:first-child {
+                  color: #42a5e9;
+                  font-size: 32px;
+                  font-weight: 700;
+                }
+                &:last-child {
+                  color: rgb(204, 204, 204);
+                  padding-left: 5px;
+                  font-size: 19px;
+                }
               }
             }
           }
         }
-        .right-data {
-          margin-left: auto;
+        .echarts {
+          width: 100%;
+          height: 700px;
+          margin-top: 20px;
+        }
+      }
+      .home-top-right {
+        width: 30%;
+        .userRetentionEchart {
+          width: 100%;
+          height: 280px;
           display: flex;
-          align-items: center;
-          .left-icon {
-            color: rgb(65, 71, 78);
-            font-size: 45px;
-            margin-right: 5px;
+          .leftEchart {
+            width: 50%;
           }
-          .data {
-            .title {
-              font-size: 20px;
-            }
-          }
-          .dataSize {
-            span {
-              &:first-child {
-                color: #42a5e9;
-                font-size: 32px;
-                font-weight: 700;
-              }
-              &:last-child {
-                color: rgb(204, 204, 204);
-                padding-left: 5px;
-                font-size: 19px;
-              }
-            }
+          .rightEchart {
+            width: 50%;
           }
         }
-      }
-      .echarts {
-        width: 100%;
-        height: 700px;
-        margin-top: 20px;
+        .newUserEchart {
+          width: 100%;
+          height: 510px;
+          margin-top: 10px;
+        }
       }
     }
-    .home-top-right {
-      width: 30%;
-      .userRetentionEchart {
-        width: 100%;
-        height: 280px;
-        display: flex;
-        .leftEchart {
-          width: 50%;
-        }
-        .rightEchart {
-          width: 50%;
-        }
-      }
-      .newUserEchart {
-        width: 100%;
-        height: 510px;
-        margin-top: 10px;
-      }
-    }
-  }
-  .home-bottom {
-    display: flex;
-    justify-content: space-between;
-    margin-top: 35px;
-    .bottom-left {
-      width: 50%;
+    .home-bottom {
       display: flex;
       justify-content: space-between;
-      .data {
-        width: calc((100% / 4));
-        height: 200px;
+      margin-top: 35px;
+      .bottom-left {
+        width: 50%;
+        display: flex;
+        justify-content: space-between;
+        .data {
+          width: calc((100% / 4));
+          height: 200px;
+        }
       }
-    }
-    .bottom-right {
-      width: 50%;
+      .bottom-right {
+        width: 50%;
+      }
     }
   }
 }
